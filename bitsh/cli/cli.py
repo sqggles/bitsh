d23e16b018249fe25524c6f7dd0199740821577b
uhjish <ajish@rootedinsights.com>
2017-11-07 14:03:17 -0500

    added arguments to support output as parquet, sqlite3, feather, csv, tsv. it's a buffet

--- bitsh/cli/cli.py
+++ bitsh/cli/cli.py
@@ -4,8 +4,18 @@ import os
 import click
 from pbr import version
 
+import pandas as pd
+import sqlalchemy as sa
+import blaze as bz
+import odo
+import logging
+
 from bitsh.cli.base import AbstractCommand
+from bitsh import log
 from bitsh import config
+
+
+logger = log.getLogger(__name__)
 
 # Retrieve the project version from PBR.
 try:
@@ -43,31 +53,44 @@ def hello_world():
     click.echo('Hello World!')
 
 
+@click.option('-d','--dburl', help='sqlalchemy url for the database')
+@click.option('-s','--schema', default=None, help='schema to use')
+@click.option('-t','--tblname', help='table name to dump')
+@click.option('-p','--parquet', help='output as parquet files',
+              type=click.BOOL, is_flag=True, default=False)
+@click.option('-q','--sqlite', help='output as an sqlite3 db',
+              type=click.BOOL, is_flag=True, default=False)
+@click.option('-f','--feather', help='output as feather files',
+              type=click.BOOL, is_flag=True, default=False)
+@click.option('--csv', help='output as csv files',
+              type=click.BOOL, is_flag=True, default=False)
+@click.option('--tsv', help='output as tsv files',
+              type=click.BOOL, is_flag=True, default=False)
 @click.command()
-@click.option('--name')
-@click.pass_context
-def hello(ctx, name):
-    """Greet somebody."""
-    command = Hello(ctx.params, ctx.obj)
-    command.execute()
+def dumpdb(dburl, schema, tblname, parquet, sqlite, feather, csv, tsv):
+    md = sa.MetaData(dburl, schema=schema)
+    db = bz.data(md)
+    odbname = schema if schema else dburl.split('/')[-1]
+    if odbname == '' or odbname is None:
+        odbname = 'dump'
+    if not os.path.exists(odbname):
+        os.makedirs(odbname)
+    for tbl in db.data.tables.keys():
+        if sqlite:
+            sadburl = 'sqlite://%s.sqlite3.db::%s' % (odbname, tblname)
+            odo(tbl, sadburl)
+        if csv:
+            odo(tbl, '%s/%s.csv')
+        if tsv:
+            odo(tbl, '%s/%s.tsv')
+        if parquet or feather:
+            df = odo(tbl, pd.DataFrame)
+            if parquet:
+                df.to_parquet('%s/%s.parquet' % (odbname, tblname))
+            if feather:
+                import feather as ft
+                feather.write_dataframe('%s/%s.feather' % (odbname, tblname))
 
 
-class Hello(AbstractCommand):
-    """Greet somebody."""
-
-    def __init__(self, command_args, global_args):
-        """
-        Initialize the command.
-
-        :param command_args: arguments of the command
-        :param global_args: arguments of the program
-        """
-        super(Hello, self).__init__(command_args, global_args)
-
-    def _execute(self):
-        """Define the internal execution of the command."""
-        click.echo('Hey {}!'.format(self.args['name']))
-
-
-cli.add_command(hello)
 cli.add_command(hello_world)
+cli.add_command(dumpdb)
